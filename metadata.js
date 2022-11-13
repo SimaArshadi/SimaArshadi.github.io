04b592d408255323236f5edfc8a174de4b700bcc
SimaArshadi <118077154+SimaArshadi@users.noreply.github.com>
2022-11-13 22:11:31 +0330

    Create metadata.js

---
+++ metadata.js
@@ -0,0 +1,79 @@
+module.exports = {
+  name: 'Sima Arhsadi',
+  title: 'M.S. Industrial Engineering',
+  facts: {
+     'LinkedIn': `<a href="https://www.linkedin.com/in/sima-arshadi-5a113b77?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B7wjq0l5GR520nNTgUFueiA%3D%3D">
+                  <i class="fa-brands fa-linkedin-w fact-icon"></i>Sima_Arshadi
+                </a>`,
+    'Twitter': `<a href="https://twitter.com/SimaArshadi"><i class="fa-brands fa-twitter fact-icon"></i>SimaArshadi</a>`,
+    'Email': '<a href="mailto:sima.arshadi@outlook.com"><i class="fa fa-envelope fact-icon"></i>sima.arshadi@outlook.com</a>'
+  },
+  skills: [
+    ['Comedy', 100],
+    ['Love', 100],
+    ['Theater', 100],
+    ['Tragedy', 100],
+    ['Poetry', 100],
+    ['Erotic', 75],
+    ['Sonnets', 100],
+    ['Plays', 100],
+    ['Writing', 100],
+    ['Directing', 75],
+    ['Literature', 100],
+    ['Influence', 100],
+  ],
+  positions: [
+    {
+      title: 'Master of science, Industrial Engineering (System Optimization)',
+      period: 'Sep. 2015 - Sep. 2017',
+      skills: ['Mathematical programming', 'Metaheuristics', 'Matlab'],
+      contents: `
+Department of Engineering, University of Urmia
+Overall GPA: 3.19/4.0 (without thesis score) 
+Thesis: Providing a Metaheuristic Optimization Model for Patient Flow Management
+Thesis score: 3.78/4.0
+Supervisors: Dr. Ali Doniavi, Dr. Yalda Katebi
+`
+    },
+    {
+      title: 'Bachelor of science, Industrial Engineering (Industrial Production)',
+      period: 'Sep. 2009 - Sep. 2013',
+      skills: ['GAMS', 'Mathematical programming'],
+      contents: `
+Department of Engineering, University of Kurdistan
+Overall GPA: 2.88/4.0
+Final Project: Classification of stock items based on their characteristics to determine the planning and controlling inventory system in “Ehya Sanat” company
+Thesis score: 4.0/4.0
+Supervisor: Dr. Fardin Ahmadi Zar
+`
+    }
+  ],
+  experience: [
+    {
+      period: '2017-2019',
+      skills: ['theater', 'play'],
+      contents: `
+Extending a dynamic model based on the presented model in the thesis by simulating model with MATLAB programming tool in cooperation with Dr. Yalda Katebi.`
+    },
+    {
+      header: '2019-2020',
+      skills: ['poetry', 'erotic'],
+      contents: `
+Doing a project with Dr. Yalda Katebi to provide a Dynamic Flexible Job-Shop scheduling mathematical model and generating a modified multi-objective metaheuristic algorithm considering a hybrid algorithm based on MOPSO and Genetic algorithm`
+    }
+  ]
+};
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
